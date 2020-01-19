cf69ad52822584238e2f0578f589d754dd3aa0d3
javaBlessMe <57236195+javaBlessMe@users.noreply.github.com>
2020-01-19 10:49:39 +0300

    '2task'

--- lesson2.java
+++ lesson2.java
@@ -1,6 +1,9 @@
 public class lesson2 {
     public static void main(String[] args) {
-
+        int[] a = fillArray();
+        for (int b: a) {
+            System.out.println(b);
+        }
     }
     //first task
     private static int[] changeZeroOne (int[] a){
@@ -9,5 +12,14 @@ public class lesson2 {
             else a[i] =0;
         }
         return a;
+    }
+
+    //second task
+    private static int[] fillArray(){
+        int[] arr = new int[8];
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i]=i*3;
+        }
+        return arr;
     }
 }
