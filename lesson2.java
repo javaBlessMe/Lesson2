77d922cd008fd6255b62d996c750e892efda554d
javaBlessMe <57236195+javaBlessMe@users.noreply.github.com>
2020-01-19 10:38:39 +0300

    '1task'

--- lesson2.java
+++ lesson2.java
@@ -1,5 +1,13 @@
 public class lesson2 {
     public static void main(String[] args) {
-        System.out.println("mass request");
+
+    }
+    //first task
+    private static int[] changeZeroOne (int[] a){
+        for (int i = 0; i <a.length ; i++) {
+            if(a[i]==0) a[i]=1;
+            else a[i] =0;
+        }
+        return a;
     }
 }
