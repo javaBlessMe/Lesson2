c23031095cfa5d1640bd3cade64e1ade5964bce6
javaBlessMe <57236195+javaBlessMe@users.noreply.github.com>
2020-01-19 10:56:16 +0300

    '3task'

--- lesson2.java
+++ lesson2.java
@@ -1,6 +1,6 @@
 public class lesson2 {
     public static void main(String[] args) {
-        int[] a = fillArray();
+        int[] a = fillArray3();
         for (int b: a) {
             System.out.println(b);
         }
@@ -19,6 +19,15 @@ public class lesson2 {
         int[] arr = new int[8];
         for (int i = 0; i <arr.length ; i++) {
             arr[i]=i*3;
+        }
+        return arr;
+    }
+
+    //third task
+    private static int[] fillArray3(){
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]<6) arr[i]*=2;
         }
         return arr;
     }
